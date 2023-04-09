e817ca7d770d803278cb98e91219f5b2bf5554ac
karloseq <87254977+ahmar7@users.noreply.github.com>
2023-04-09 09:21:31 +0500

    final

--- src/components/homepage/bannerVideo.jsx
+++ src/components/homepage/bannerVideo.jsx
@@ -1,26 +1,26 @@
-import styles from "./banner.module.css";
-import { useEffect } from "react";
+import styles from "./banner.module.css"; 
+import './custom.css'
+import img from '../../images/home/hero3.webp'
 let BannerVideo=()=> {
-  const player = () => {
-    let vid = document.getElementById('autopl');
-   vid.setAttribute("muted","" )
-  }
-  useEffect(() => {
-    player();
-  }, []);
+ 
+  
   return (
-    <video
-    id="autopl"
-      style={{ pointerEvents: "none" }}
-      muted={true}
-      className={["image", styles.image].join(" ")}
-      autoPlay
-      loop
-      playsinline
-    >
-      <source src="/assets/video/hero3.webm" type="video/webm" />
-      <source src="/assets/video/hero3.mp4" type="video/mp4" />
-    </video>
+   <>
+      <video
+        id="autopl"
+        style={{ pointerEvents: "none" }}
+        muted={true}
+        className={["image desk-only", styles.image].join(" ")}
+        autoPlay
+        loop
+        playsinline
+      >
+        <source src="/assets/video/hero3.webm" type="video/webm" />
+        <source src="/assets/video/hero3.mp4" type="video/mp4" />
+      </video>
+      <img className="mbl-only" src={img} alt="" />
+       
+   </>
   );
 }
 export default BannerVideo;
