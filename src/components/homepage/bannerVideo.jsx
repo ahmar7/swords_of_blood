9f17240a3b86fcc246076610aaddefa62dadf00c
karloseq <87254977+ahmar7@users.noreply.github.com>
2023-04-14 05:40:41 +0500

    update

--- src/components/homepage/bannerVideo.jsx
+++ src/components/homepage/bannerVideo.jsx
@@ -11,9 +11,7 @@ let BannerVideo=()=> {
         style={{ pointerEvents: "none" }}
          
         className={["image desk-only", styles.image].join(" ")}
-        autoPlay
-        loop 
-        playsinline
+       
       >
         <source   src="/assets/video/hero3.webm" type="video/webm" />
         <source  src="/assets/video/hero3.mp4" type="video/mp4" />
